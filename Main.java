199d3ba1f22cb5c11a871a3720ffa096b695b265
sundarsv <sundarsv@gmail.com>
2018-04-19 11:18:41 +0530

    first ever push.  Second attempt though

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] arg) {
+  //Declaring and initializing variables
+  int distance = 0;
+  int resttime = 0;
+  //Updating a Variable
+
+  distance = distance + 200;
+  distance = distance + 200;
+  distance = distance + 100;
+  resttime = resttime + 10;
+  resttime = resttime + 10;
+  resttime = resttime + 5;
+
+  //Printing the values
+  System.out.println(distance);
+
+  }
+}
